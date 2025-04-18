b70025619a6b278e460a65414132d3957af0b1e6
cmoynard <cmoynard.etude@gmail.com>
2025-04-18 14:12:06 +0200

    feat: title for block group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import HomeCarousel from "./_components/HomeCarousel";
 import FusionBlock from "./_components/FusionBlock";
+import LogoRG from "../../public/logo-rg-vide-mais-blanc.png";
+import Image from "next/image";
 
 type HomeProps = {};
 
@@ -13,7 +15,7 @@ export default function Home({}: HomeProps) {
       <div className="flex flex-col gap-8 px-4 mt-8">
         <FusionBlock isImageLeft={true}>
           <div className="space-y-4">
-            <h2 className="text-3xl font-bold">Titre de la section</h2>
+            <h2 className="text-5xl font-bold">Titre de la section</h2>
             <p className="text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -22,15 +24,21 @@ export default function Home({}: HomeProps) {
             </p>
           </div>
         </FusionBlock>
+
+        <div className="flex text-6xl font-bold items-center">
+          <Image
+            src={LogoRG}
+            alt="Rose Griffon Logo"
+            height={128}
+            width={128}
+          />
+          Nos projets
+        </div>
+
         <FusionBlock isImageLeft={false}>
           <div className="space-y-4">
-            <h2 className="text-3xl font-bold">Titre de la section</h2>
-            <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
+            <h2 className="text-5xl font-bold">Projet 1</h2>
+            <p className="text-lg">Ceci est un projet de test.</p>
           </div>
         </FusionBlock>
       </div>
