fe46e3ca216b2d8218ad1a84ca1a96403c7d2753
cmoynard <cmoynard.etude@gmail.com>
2025-04-22 23:22:24 +0200

    added RG banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import FusionBlock from "./_components/FusionBlock";
 import LogoRG from "../../public/logo-rg-vide-mais-blanc.png";
 import Image from "next/image";
 import { ArrowRight } from "lucide-react";
+import BanniereRG from "@public/RG_screens_twitch_ethan.png";
 
 type HomeProps = {};
 
@@ -14,14 +15,15 @@ export default function Home({}: HomeProps) {
     <div className="flex flex-col mb-8">
       <HomeCarousel />
       <div className="flex flex-col gap-8 px-4 mt-8">
-        <FusionBlock isImageLeft={true}>
+        <FusionBlock isImageLeft={true} image={BanniereRG.src}>
           <div className="space-y-4">
-            <h2 className="text-5xl font-bold">Titre de la section</h2>
+            <h2 className="text-5xl font-bold">Qu'est-ce que Rose Griffon ?</h2>
             <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              Rose Griffon est une association française régie par la loi 1901,
+              ayant pour vocation de rassembler les principaux acteurs de la
+              communauté Inazuma Eleven ainsi que l’ensemble des passionnés sous
+              une même bannière. Elle s’engage à valoriser à la fois la scène
+              artistique liée à cette licence et son univers compétitif.
             </p>
           </div>
         </FusionBlock>
