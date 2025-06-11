848b5461ee00cbc9d177a7c8588ee52272afe71e
cmoynard <cmoynard.etude@gmail.com>
2025-06-11 12:28:16 +0200

    fixed bad files overlapping

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "cdn.cosmicjs.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
     ],
     dangerouslyAllowSVG: true,
   },
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL("https://placehold.co/600x400")],
-  },
-};
-
-export default nextConfig;
