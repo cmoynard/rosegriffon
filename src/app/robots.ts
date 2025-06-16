bd4c2690bb93a686b67dd0d162213acd4a2ef0a0
cmoynard <cmoynard.etude@gmail.com>
2025-06-16 23:27:55 +0200

    test seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,13 @@
 import { MetadataRoute } from "next";
 
 // Définition du domaine du site
-const baseUrl = "https://rosegriffon.fr";
+const baseUrl = "https://www.rosegriffon.fr";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/admin/"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
